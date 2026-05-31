c97763db553bae0792d226f4420c5e9ddd3df5c8
DirekDontol <rextoretto25@gmail.com>
2026-05-31 16:04:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// Animasi sederhana saat muncul
+document.querySelectorAll('.glass').forEach((el) => {
+    el.style.opacity = 0;
+    el.style.transition = 'opacity 1s';
+    setTimeout(() => el.style.opacity = 1, 500);
+});
 
+// Integrasi form
+document.getElementById('saranForm').onsubmit = (e) => {
+    e.preventDefault();
+    alert("Terima kasih saran berharganya, Dyyrex!");
+};
